7377da5a404769b903e886d4c49cfafa823f5f9f
sakebook <sakebook@gmail.com>
2018-11-01 21:01:46 +0900

    Add Fuel and Moshi

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    kotlin("jvm") version "1.2.71"
+    kotlin("jvm") version "1.3.0"
 }
 
 group = "net.jxpress"
@@ -10,10 +10,14 @@ version = "0.0.1"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
     compile(kotlin("stdlib-jdk8"))
+    compile("com.github.kittinunf.fuel", "fuel", "1.16.0")
+    compile("com.github.kittinunf.fuel", "fuel-moshi", "1.16.0")
+    compile("com.squareup.moshi", "moshi-kotlin", "1.7.0")
     testCompile("junit", "junit", "4.12")
 }
 
